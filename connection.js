b62823457e2667b0bcd8d1a905588eb0ef25273d
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-04-16 21:32:12 -0700

    The only solution to the circular redundancy seems to have been to combine all of the js files - everything functions - video tomorrow

--- connection.js
+++ connection.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
 const mysql = require('mysql2');
 require('dotenv').config();
 
@@ -26,6 +28,161 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT} - You better run and catch it!`);
+    mainMenu();
 });
 
-module.exports = db;
+function exit() {
+    console.log('Later, skater!');
+    process.exit();
+}
+
+const actions = {
+    'View all departments': viewAllDepartments,
+    'View all roles': viewAllRoles,
+    'View all employees': viewAllEmployees,
+    'Add a department': addDepartment,
+    'Add a role': addRole,
+    'Add an employee': addEmployee,
+    'Update an employee role': updateEmployeeRole,
+    'Return to main menu': mainMenu,
+    'Exit': exit
+};
+
+function mainMenu() {
+    inquirer.prompt({
+        type: 'list',
+        name: 'action',
+        message: 'Whatchagonnado?',
+        choices: Object.keys(actions)
+    }).then(answer => {
+        actions[answer.action]();
+    });
+}
+
+function viewAllDepartments() {
+    db.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainMenu();
+    });
+}
+
+function viewAllRoles() {
+    db.query('SELECT * FROM role', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainMenu();
+    });
+}
+
+function viewAllEmployees() {
+    db.query('SELECT * FROM employee', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainMenu();
+    });
+}
+
+function addDepartment() {
+    inquirer.prompt({
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of the department you would like to add?'
+    }).then(answer => {
+    db.query('INSERT INTO department SET ?', {
+        name: answer.name },
+        (err, res) => {
+            if (err) throw err;
+            console.log('Department has been added to the database.');
+            console.table(res);
+            mainMenu();
+        });
+})
+}
+
+function addRole() {
+    inquirer.prompt([
+        {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of the role you would like to add?'
+    },
+    {
+        type: 'input',
+        name: 'salary',
+        message: 'What is the salary of the new role?'
+    },
+    {
+        type: 'input',
+        name: 'department_id',
+        message: 'To which department does the new role belong?'
+    }
+    ]).then(answer => {
+    db.query('INSERT INTO role SET ?', [ {
+        title: answer.title, salary: answer.salary, department_id: answer.department_id } ],
+        (err, res) => {
+            if (err) throw err;
+            console.log('Role has been added to the database.');
+            console.table(res);
+            mainMenu();
+        });
+})
+}
+
+function addEmployee() {
+    inquirer.prompt([
+    {
+        type: 'input',
+        name: 'first_name',
+        message: 'What is the first name of the employee you would like to add?'
+    },
+    {
+        type: 'input',
+        name: 'last_name',
+        message: 'What is the last name of the employee you would like to add?'
+    },
+    {
+        type: 'input',
+        name: 'role_id',
+        message: 'What is the role ID of the employee you would like to add?'
+    },
+    {
+        type: 'input',
+        name: 'manager_id',
+        message: 'What is the manager ID of the employee you would like to add?'
+    }
+    ]).then(answer => {
+    db.query('INSERT INTO employee SET ?', 
+    [ { first_name: answer.first_name, last_name: answer.last_name, role_id: answer.role_id, manager_id: answer.manager_id } ],
+        (err, res) => {
+            if (err) throw err;
+            console.log('Employee has been added to the database.');
+            console.table(res);
+            mainMenu();
+        });
+})
+}
+
+function updateEmployeeRole() {
+    inquirer.prompt([
+    {
+        type: 'input',
+        name: 'employee_id',
+        message: 'What is the ID of the employee whose role you would like to update?'
+    },
+    {
+        type: 'input',
+        name: 'role_id',
+        message: 'What is the new role ID of the employee you would like to update?'
+    }
+    ]).then(answer => {
+    db.query('UPDATE employee SET role_id = ? WHERE id = ?',
+    [answer.role_id, answer.employee_id],
+    (err, res) => {
+        if (err) throw err;
+        console.log('Employee role has been updated.');
+        console.table(res);
+        mainMenu();
+    });
+})
+}
